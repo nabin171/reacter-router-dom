8551a12e50e12ac8527f2d715d71dd5daf51907a
nabin171 <karki0008@gmail.com>
2026-01-22 15:49:04 +0545

    CI has been changeg

--- Desktop/react router dom/my-react-app/src/App.jsx
+++ Desktop/react router dom/my-react-app/src/App.jsx
@@ -1,10 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./components/home";
 import About from "./components/About";
 import User from "./components/User";
-
 function App() {
   const router = createBrowserRouter([
     {
@@ -21,7 +19,6 @@ function App() {
       element: (
         <>
           <Navbar />
-          <Home />,
         </>
       ),
     },
